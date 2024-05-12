351491e4b051924bf5ed54ad7727dc538cb1ad7c
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-12 12:26:20 -0500

    Actualizar gata.sh

--- gata.sh
+++ gata.sh
@@ -331,5 +331,5 @@ _______░▒▓██
 _____░▒▓██\n\e[0m"
     
 sleep 15 && pkill mpv > /dev/null 2>&1 && sleep 2 && rm -f /data/data/com.termux/files/home/"$random_mp3" &
-echo -e "\033[01;32m\033[01mIniciando GataBot!!\nStarting CatBot!!\n\033[0m"
+echo -e "\033[01;32m\033[01mIniciando VegettaBot!!\nStarting VegettaBot!!\n\033[0m"
 npm start
