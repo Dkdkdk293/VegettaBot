ee6e8e3aecc2894a4c88f29f574d3140e207d59d
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-05-13 18:24:39 -0500

    Update rpg-rangos.js

--- plugins/rpg-rangos.js
+++ plugins/rpg-rangos.js
@@ -67,109 +67,108 @@ let username = conn.getName(who)
 //let user = global.db.data.users[m.sender]
 //user.registered = false
 
-let menu = `
-╭━━━〔 𝙍𝘼𝙉𝙂𝙊𝙎 | 𝙍𝙊𝙇 〕━━━⬣
+let menu = `╭━━━〔 𝙍𝘼𝙉𝙂𝙊𝙎 | 𝙍𝙊𝙇 〕━━━⬣
 𝙉𝙊𝙈𝘽𝙍𝙀
 ${username}
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 𝙏𝙐  𝙍𝘼𝙉𝙂𝙊 𝘼𝘾𝙏𝙐𝘼𝙇
 ${role}
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-👑 *∞ ÉLITE GLOBAL I* 💎🏁
-👑 *∞ ÉLITE GLOBAL II* 💎🏁
-👑 *∞ ÉLITE GLOBAL III* 💎🏁
-👑 *∞ ÉLITE GLOBAL IV* 💎🏁
-👑 *∞ ÉLITE GLOBAL V* 💎🏁
+👑 *∞ 𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 I* 💎🏁
+👑 *∞ 𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 II* 💎🏁
+👑 *∞ 𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 III* 💎🏁
+👑 *∞ 𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 IV* 💎🏁
+👑 *∞ 𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 V* 💎🏁
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-👑 *ÉLITE GLOBAL I* 🏁
-👑 *ÉLITE GLOBAL II* 🏁
-👑 *ÉLITE GLOBAL III* 🏁
-👑 *ÉLITE GLOBAL IV* 🏁
-👑 *ÉLITE GLOBAL V* 🏁
+👑 *𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 I* 🏁
+👑 *𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 II* 🏁
+👑 *𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 III* 🏁
+👑 *𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 IV* 🏁
+👑 *𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 V* 🏁
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*TOP ASTRAL I* ⚜️🔱
-*TOP ASTRAL II* ⚜️🔱
-*TOP ASTRAL III* ⚜️🔱
-*TOP ASTRAL IV* ⚜️🔱
-*TOP ASTRAL V* ⚜️🔱
+*𝙏𝙊𝙋 𝘼𝙎𝙏𝙍𝘼𝙇 I* ⚜️🔱
+*𝙏𝙊𝙋 𝘼𝙎𝙏𝙍𝘼𝙇 II* ⚜️🔱
+*𝙏𝙊𝙋 𝘼𝙎𝙏𝙍𝘼𝙇 III* ⚜️🔱
+*𝙏𝙊𝙋 𝘼𝙎𝙏𝙍𝘼𝙇 IV* ⚜️🔱
+*𝙏𝙊𝙋 𝘼𝙎𝙏𝙍𝘼𝙇 V* ⚜️🔱
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*ESTELAR I* ☄️
-*ESTELAR II* ☄️
-*ESTELAR III* ☄️
-*ESTELAR IV* ☄️
-*ESTELAR V* ☄️
+*𝙀𝙎𝙏𝙀𝙇𝘼𝙍 I* ☄️
+*𝙀𝙎𝙏𝙀𝙇𝘼𝙍 II* ☄️
+*𝙀𝙎𝙏𝙀𝙇𝘼𝙍 III* ☄️
+*𝙀𝙎𝙏𝙀𝙇𝘼𝙍 IV* ☄️
+*𝙀𝙎𝙏𝙀𝙇𝘼𝙍 V* ☄️
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*LEYENDA I* 🏆
-*LEYENDA II* 🏆
-*LEYENDA III* 🏆
-*LEYENDA IV* 🏆
-*LEYENDA V* 🏆
+*𝙇𝙀𝙔𝙀𝙉𝘿𝘼 I* 🏆
+*𝙇𝙀𝙔𝙀𝙉𝘿𝘼 II* 🏆
+*𝙇𝙀𝙔𝙀𝙉𝘿𝘼 III* 🏆
+*𝙇𝙀𝙔𝙀𝙉𝘿𝘼 IV* 🏆
+*𝙇𝙀𝙔𝙀𝙉𝘿𝘼 V* 🏆
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*LEGENDARIO(A) I* 🛡️
-*LEGENDARIO(A) II* 🛡️
-*LEGENDARIO(A) III* 🛡️
-*LEGENDARIO(A) IV* 🛡️
-*LEGENDARIO(A) V* 🛡️
+*𝙇𝙀𝙂𝙀𝙉𝘿𝘼𝙍𝙄𝙊(𝘼) I* 🛡️
+*𝙇𝙀𝙂𝙀𝙉𝘿𝘼𝙍𝙄𝙊(𝘼) II* 🛡️
+*𝙇𝙀𝙂𝙀𝙉𝘿𝘼𝙍𝙄𝙊(𝘼) III* 🛡️
+*𝙇𝙀𝙂𝙀𝙉𝘿𝘼𝙍𝙄𝙊(𝘼) IV* 🛡️
+*𝙇𝙀𝙂𝙀𝙉𝘿𝘼𝙍𝙄𝙊(𝘼) V* 🛡️
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*SUPER PRO I* 🎩
-*SUPER PRO II* 🎩
-*SUPER PRO III* 🎩
-*SUPER PRO IV* 🎩
-*SUPER PRO V* 🎩
+*𝙎𝙐𝙋𝙀𝙍 𝙋𝙍𝙊 I* 🎩
+*𝙎𝙐𝙋𝙀𝙍 𝙋𝙍𝙊 II* 🎩
+*𝙎𝙐𝙋𝙀𝙍 𝙋𝙍𝙊 III* 🎩
+*𝙎𝙐𝙋𝙀𝙍 𝙋𝙍𝙊 IV* 🎩
+*𝙎𝙐𝙋𝙀𝙍 𝙋𝙍𝙊 V* 🎩
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*PRO EN GATABOT I* 😼
-*PRO EN GATABOT II* 😼
-*PRO EN GATABOT III* 😼
-*PRO EN GATABOT IV* 😼
-*PRO EN GATABOT V* 😼
+*𝙋𝙍𝙊 𝙀𝙉 𝙑𝙀𝙂𝙀𝙏𝙏𝘼𝘽𝙊𝙏 I* 🔰
+*𝙋𝙍𝙊 𝙀𝙉 𝙑𝙀𝙂𝙀𝙏𝙏𝘼𝘽𝙊𝙏 II* 🔰
+*𝙋𝙍𝙊 𝙀𝙉 𝙑𝙀𝙂𝙀𝙏𝙏𝘼𝘽𝙊𝙏 III* 🔰
+*𝙋𝙍𝙊 𝙀𝙉 𝙑𝙀𝙂𝙀𝙏𝙏𝘼𝘽𝙊𝙏 IV* 🔰
+*𝙋𝙍𝙊 𝙀𝙉 𝙑𝙀𝙂𝙀𝙏𝙏𝘼𝘽𝙊𝙏 V* 🔰
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*DIAMANTE I* 💎
-*DIAMANTE II* 💎
-*DIAMANTE III* 💎
-*DIAMANTE IV* 💎
-*DIAMANTE V* 💎
+*𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀 I* 💎
+*𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀 II* 💎
+*𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀 III* 💎
+*𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀 IV* 💎
+*𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀 V* 💎
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*ORO I* 🏅
-*ORO II* 🏅
-*ORO III* 🏅
-*ORO IV* 🏅
-*ORO V* 🏅
+*𝙊𝙍𝙊 I* 🏅
+*𝙊𝙍𝙊 II* 🏅
+*𝙊𝙍𝙊 III* 🏅
+*𝙊𝙍𝙊 IV* 🏅
+*𝙊𝙍𝙊 V* 🏅
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*PLATA I* 🔮
-*PLATA II* 🔮
-*PLATA III* 🔮
-*PLATA IV* 🔮
-*PLATA V* 🔮
+*𝙋𝙇𝘼𝙏𝘼 I* 🔮
+*𝙋𝙇𝘼𝙏𝘼 II* 🔮
+*𝙋𝙇𝘼𝙏𝘼 III* 🔮
+*𝙋𝙇𝘼𝙏𝘼 IV* 🔮
+*𝙋𝙇𝘼𝙏𝘼 V* 🔮
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*IRON I* 🦾
-*IRON II* 🦾
-*IRON III* 🦾
-*IRON IV* 🦾
-*IRON V* 🦾
+*𝙄𝙍𝙊𝙉 I* 🦾
+*𝙄𝙍𝙊𝙉 II* 🦾
+*𝙄𝙍𝙊𝙉 III* 🦾
+*𝙄𝙍𝙊𝙉 IV* 🦾
+*𝙄𝙍𝙊𝙉 V* 🦾
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*MAESTRO(A) I* ⚒️
-*MAESTRO(A) II* ⚒️
-*MAESTRO(A) III* ⚒️
-*MAESTRO(A) IV* ⚒️
-*MAESTRO(A) V* ⚒️
+*𝙈𝘼𝙀𝙎𝙏𝙍𝙊(𝘼) I* ⚒️
+*𝙈𝘼𝙀𝙎𝙏𝙍𝙊(𝘼) II* ⚒️
+*𝙈𝘼𝙀𝙎𝙏𝙍𝙊(𝘼) III* ⚒️
+*𝙈𝘼𝙀𝙎𝙏𝙍𝙊(𝘼) IV* ⚒️
+*𝙈𝘼𝙀𝙎𝙏𝙍𝙊(𝘼) V* ⚒️
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*EXPLORADOR(A) I* 🪓
-*EXPLORADOR(A) II* 🪓
-*EXPLORADOR(A) III* 🪓
-*EXPLORADOR(A) IV* 🪓
-*EXPLORADOR(A) V* 🪓
+*𝙀𝙓𝙋𝙇𝙊𝙍𝘼𝘿𝙊𝙍(𝘼) I* 🪓
+*𝙀𝙓𝙋𝙇𝙊𝙍𝘼𝘿𝙊𝙍(𝘼) II* 🪓
+*𝙀𝙓𝙋𝙇𝙊𝙍𝘼𝘿𝙊𝙍(𝘼) III* 🪓
+*𝙀𝙓𝙋𝙇𝙊𝙍𝘼𝘿𝙊𝙍(𝘼) IV* 🪓
+*𝙀𝙓𝙋𝙇𝙊𝙍𝘼𝘿𝙊𝙍(𝘼) V* 🪓
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*APRENDIS I* 🪚
-*APRENDIS II* 🪚
-*APRENDIS III* 🪚
-*APRENDIS IV* 🪚
-*APRENDIS V* 🪚
+*𝘼𝙋𝙍𝙀𝙉𝘿𝙄𝙎 I* 🪚
+*𝘼𝙋𝙍𝙀𝙉𝘿𝙄𝙎 II* 🪚
+*𝘼𝙋𝙍𝙀𝙉𝘿𝙄𝙎 III* 🪚
+*𝘼𝙋𝙍𝙀𝙉𝘿𝙄𝙎 IV* 🪚
+*𝘼𝙋𝙍𝙀𝙉𝘿𝙄𝙎 V* 🪚
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*NOVATO(A) I* 🪤
-*NOVATO(A) II* 🪤
-*NOVATO(A) III* 🪤
-*NOVATO(A) IV* 🪤
-*NOVATO(A) V* 🪤
+*𝙉𝙊𝙑𝘼𝙏𝙊(𝘼) I* 🪤
+*𝙉𝙊𝙑𝘼𝙏𝙊(𝘼) II* 🪤
+*𝙉𝙊𝙑𝘼𝙏𝙊(𝘼) III* 🪤
+*𝙉𝙊𝙑𝘼𝙏𝙊(𝘼) IV* 🪤
+*𝙉𝙊𝙑𝘼𝙏𝙊(𝘼) V* 🪤
 ╰━━━━━━━━━━━━━━━━━━━⬣`.trim()
 await conn.sendFile(m.chat, pp, 'gata.mp4', menu)
 //conn.sendHydrated(m.chat, menu, `𝙍𝘼𝙉𝙂𝙊𝙎 | ${wm}`, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [['𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 💫', '.allmenu'],['𝙏𝙤𝙥𝙨 | 𝙍𝙖𝙣𝙠𝙞𝙣𝙜 🏆', `${usedPrefix}top`],['𝙈𝙚𝙣𝙪 𝙋𝙧𝙞𝙣𝙘𝙞𝙥𝙖𝙡 | 𝙈𝙖𝙞𝙣 𝙢𝙚𝙣𝙪 ⚡', '#menu']], m,)
