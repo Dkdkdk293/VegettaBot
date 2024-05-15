dc1ec87bd68ea3bcf0b08f6cb877ce6346961068
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-14 19:28:17 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ║ ✅ ${uptime} ⌛ ║ ⒼⒷ 𓃠 ${[`#donar #menu #serbot #gruposgb #fantasy  By GLOBAL-GB`, `#estado #menu #jadibot #cuentagatabot 🐈`, `#infobot #owner #ping #fy 💻 By: Global-GB 🐈`].getRandom()}`
+let bio = `${global.packname} ║ ✅ ${uptime} ⌛`].getRandom()}`
 await this.updateProfileStatus(bio).catch(_ => _)
 //await this.updateProfilePicture(gataImg.getRandom()).catch(_ => _)
 setting.status = new Date() * 1
